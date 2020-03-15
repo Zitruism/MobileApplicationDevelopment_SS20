ae5a25e466e5308789eed1743ca26ccd531ba20f
Philipp Thiele <philipp-christopher.thiele@stud.th-luebeck.de>
2020-03-15 16:44:47 +0100

    Initial commit

---
+++ app/src/main/java/de/zitruism/thl_todo_liste/MainActivity.java
@@ -0,0 +1,14 @@
+package de.zitruism.thl_todo_liste;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
